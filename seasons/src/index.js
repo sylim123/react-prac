18d0f13db387383c3094d361e296f31c905472b0
seyoon.Lim <sylim123@naver.com>
2020-03-27 19:31:00 +0900

    finished the section 5

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+    constructor(props) {
+        super(props);
 
-        return <div>Latitude: </div>;
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // we called setstate!!!
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    // React says we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
     }
 }
 
